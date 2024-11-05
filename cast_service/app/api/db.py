38976bec794f7834997fe49920c4f1ec88692536
franck owen <fxfrancky@outlook.com>
2024-11-06 00:24:29 +0100

    fixing port

--- cast_service/app/api/db.py
+++ cast_service/app/api/db.py
@@ -9,9 +9,10 @@ from databases import Database
 POSTGRES_USER : str = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB : str = os.getenv("POSTGRES_DB")
-POSTGRES_SERVICE = "castdb-service"
-POSTGRES_PORT = int("5432")
+POSTGRES_SERVICE = 'castdb-service'
+POSTGRES_PORT = '5432'
 DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVICE}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
 
 engine = create_engine(DATABASE_URI)
 metadata = MetaData()
